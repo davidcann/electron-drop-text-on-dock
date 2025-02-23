26f0657568078821645df0d4a30ac3be0ac14391
David Cann <david@davidcann.com>
2025-02-23 06:23:45 -0800

    Fix recursion bug when building demo with electron-forge

--- demo/forge.config.js
+++ demo/forge.config.js
@@ -1,3 +1,6 @@
+const fs = require("fs-extra");
+const path = require("path");
+
 module.exports = {
 	packagerConfig: {
 		extendInfo: {
@@ -10,6 +13,28 @@ module.exports = {
 					},
 				},
 			],
+		},
+		prune: true,
+	},
+	hooks: {
+		packageAfterCopy: async (config, buildPath, electronVersion, platform, arch) => {
+			// This is a hack to prevent recursive copying of the demo folder
+			try {
+				const sourcePath = path.resolve(__dirname, "..");
+				const destinationPath = path.join(buildPath, "node_modules", "electron-drop-text-on-dock");
+				await fs.copy(sourcePath, destinationPath, {
+					filter: (src) => {
+						const shouldCopy = !src.includes(path.join(sourcePath, "demo"));
+						if (!shouldCopy) {
+							console.log(`Excluding ${src}`);
+						}
+						return shouldCopy;
+					},
+				});
+				console.log("Files copied successfully.");
+			} catch (error) {
+				throw new Error("Error during packageAfterCopy hook:", error);
+			}
 		},
 	},
 	makers: [
